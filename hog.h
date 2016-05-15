11f13d5d31ddcb86a06d53d38d28686842429977
lolski <soulmagic13@gmail.com>
2016-05-15 12:07:14 +0800

    fix formatting

--- hog.h
+++ hog.h
@@ -62,15 +62,15 @@ namespace hog {
     float*** gradientStrengths = new float**[cells_in_y_dir];
     int** cellUpdateCounter   = new int*[cells_in_y_dir];
     for (int y=0; y<cells_in_y_dir; y++) {
-        gradientStrengths[y] = new float*[cells_in_x_dir];
-        cellUpdateCounter[y] = new int[cells_in_x_dir];
-        for (int x=0; x<cells_in_x_dir; x++) {
-            gradientStrengths[y][x] = new float[gradientBinSize];
-            cellUpdateCounter[y][x] = 0;
+      gradientStrengths[y] = new float*[cells_in_x_dir];
+      cellUpdateCounter[y] = new int[cells_in_x_dir];
+      for (int x=0; x<cells_in_x_dir; x++) {
+        gradientStrengths[y][x] = new float[gradientBinSize];
+        cellUpdateCounter[y][x] = 0;
 
-            for (int bin=0; bin<gradientBinSize; bin++)
-                gradientStrengths[y][x][bin] = 0.0;
-        }
+        for (int bin=0; bin<gradientBinSize; bin++)
+          gradientStrengths[y][x][bin] = 0.0;
+      }
     }
 
     // nr of blocks = nr of cells - 1
@@ -84,109 +84,105 @@ namespace hog {
     int celly = 0;
 
     for (int blockx=0; blockx<blocks_in_x_dir; blockx++) {
-        for (int blocky=0; blocky<blocks_in_y_dir; blocky++) {
-            // 4 cells per block ...
-            for (int cellNr=0; cellNr<4; cellNr++) {
-                // compute corresponding cell nr
-                cellx = blockx;
-                celly = blocky;
-                if (cellNr==1) celly++;
-                if (cellNr==2) cellx++;
-                if (cellNr==3)
-                {
-                    cellx++;
-                    celly++;
-                }
+      for (int blocky=0; blocky<blocks_in_y_dir; blocky++) {
+        // 4 cells per block ...
+        for (int cellNr=0; cellNr<4; cellNr++) {
+            // compute corresponding cell nr
+            cellx = blockx;
+            celly = blocky;
+            if (cellNr==1) celly++;
+            if (cellNr==2) cellx++;
+            if (cellNr==3)
+            {
+                cellx++;
+                celly++;
+            }
 
-                for (int bin=0; bin<gradientBinSize; bin++) {
-                    float gradientStrength = descriptorValues[ descriptorDataIdx ];
-                    descriptorDataIdx++;
+            for (int bin=0; bin<gradientBinSize; bin++) {
+                float gradientStrength = descriptorValues[ descriptorDataIdx ];
+                descriptorDataIdx++;
 
-                    gradientStrengths[celly][cellx][bin] += gradientStrength;
+                gradientStrengths[celly][cellx][bin] += gradientStrength;
 
-                } // for (all bins)
+            } // for (all bins)
 
+            // note: overlapping blocks lead to multiple updates of this sum!
+            // we therefore keep track how often a cell was updated,
+            // to compute average gradient strengths
+            cellUpdateCounter[celly][cellx]++;
+        } // for (all cells)
 
-                // note: overlapping blocks lead to multiple updates of this sum!
-                // we therefore keep track how often a cell was updated,
-                // to compute average gradient strengths
-                cellUpdateCounter[celly][cellx]++;
-
-            } // for (all cells)
-
-
-        } // for (all block x pos)
+      } // for (all block x pos)
     } // for (all block y pos)
 
 
     // compute average gradient strengths
     for (celly=0; celly<cells_in_y_dir; celly++) {
-        for (cellx=0; cellx<cells_in_x_dir; cellx++) {
+      for (cellx=0; cellx<cells_in_x_dir; cellx++) {
+        float NrUpdatesForThisCell = (float)cellUpdateCounter[celly][cellx];
 
-            float NrUpdatesForThisCell = (float)cellUpdateCounter[celly][cellx];
-
-            // compute average gradient strenghts for each gradient bin direction
-            for (int bin=0; bin<gradientBinSize; bin++) {
-                gradientStrengths[celly][cellx][bin] /= NrUpdatesForThisCell;
-            }
+        // compute average gradient strenghts for each gradient bin direction
+        for (int bin=0; bin<gradientBinSize; bin++) {
+          gradientStrengths[celly][cellx][bin] /= NrUpdatesForThisCell;
         }
+      }
     }
 
     // draw cells
     for (celly=0; celly<cells_in_y_dir; celly++) {
-        for (cellx=0; cellx<cells_in_x_dir; cellx++) {
-            int drawX = cellx * cellSize;
-            int drawY = celly * cellSize;
+      for (cellx=0; cellx<cells_in_x_dir; cellx++) {
+        int drawX = cellx * cellSize;
+        int drawY = celly * cellSize;
 
-            int mx = drawX + cellSize/2;
-            int my = drawY + cellSize/2;
+        int mx = drawX + cellSize/2;
+        int my = drawY + cellSize/2;
 
-            rectangle(visu, Point((int)(drawX*zoomFac), (int)(drawY*zoomFac)), Point((int)((drawX+cellSize)*zoomFac), (int)((drawY+cellSize)*zoomFac)), Scalar(100,100,100), 1);
+        rectangle(visu, Point((int)(drawX*zoomFac), (int)(drawY*zoomFac)), Point((int)((drawX+cellSize)*zoomFac), (int)((drawY+cellSize)*zoomFac)), Scalar(100,100,100), 1);
 
-            // draw in each cell all 9 gradient strengths
-            for (int bin=0; bin<gradientBinSize; bin++) {
-                float currentGradStrength = gradientStrengths[celly][cellx][bin];
+        // draw in each cell all 9 gradient strengths
+        for (int bin=0; bin<gradientBinSize; bin++) {
+            float currentGradStrength = gradientStrengths[celly][cellx][bin];
 
-                // no line to draw?
-                if (currentGradStrength==0)
-                    continue;
+            // no line to draw?
+            if (currentGradStrength==0)
+                continue;
 
-                float currRad = bin * radRangeForOneBin + radRangeForOneBin/2;
+            float currRad = bin * radRangeForOneBin + radRangeForOneBin/2;
 
-                float dirVecX = cos( currRad );
-                float dirVecY = sin( currRad );
-                float maxVecLen = (float)(cellSize/2.f);
-                float scale = 2.5; // just a visualization scale, to see the lines better
+            float dirVecX = cos( currRad );
+            float dirVecY = sin( currRad );
+            float maxVecLen = (float)(cellSize/2.f);
+            float scale = 2.5; // just a visualization scale, to see the lines better
 
-                // compute line coordinates
-                float x1 = mx - dirVecX * currentGradStrength * maxVecLen * scale;
-                float y1 = my - dirVecY * currentGradStrength * maxVecLen * scale;
-                float x2 = mx + dirVecX * currentGradStrength * maxVecLen * scale;
-                float y2 = my + dirVecY * currentGradStrength * maxVecLen * scale;
+            // compute line coordinates
+            float x1 = mx - dirVecX * currentGradStrength * maxVecLen * scale;
+            float y1 = my - dirVecY * currentGradStrength * maxVecLen * scale;
+            float x2 = mx + dirVecX * currentGradStrength * maxVecLen * scale;
+            float y2 = my + dirVecY * currentGradStrength * maxVecLen * scale;
 
-                // draw gradient visualization
-                line(visu, Point((int)(x1*zoomFac),(int)(y1*zoomFac)), Point((int)(x2*zoomFac),(int)(y2*zoomFac)), Scalar(0,255,0), 1);
+            // draw gradient visualization
+            line(visu, Point((int)(x1*zoomFac),(int)(y1*zoomFac)), Point((int)(x2*zoomFac),(int)(y2*zoomFac)), Scalar(0,255,0), 1);
 
-            } // for (all bins)
+        } // for (all bins)
 
-        } // for (cellx)
+      } // for (cellx)
     } // for (celly)
 
 
     // don't forget to free memory allocated by helper data structures!
     for (int y=0; y<cells_in_y_dir; y++) {
-        for (int x=0; x<cells_in_x_dir; x++) {
-            delete[] gradientStrengths[y][x];
-        }
-        delete[] gradientStrengths[y];
-        delete[] cellUpdateCounter[y];
+      for (int x=0; x<cells_in_x_dir; x++) {
+        delete[] gradientStrengths[y][x];
+      }
+      delete[] gradientStrengths[y];
+      delete[] cellUpdateCounter[y];
     }
     delete[] gradientStrengths;
     delete[] cellUpdateCounter;
 
     return visu;
 
-    } // visualizeHog
+  } // visualizeHog
 }
 
 
